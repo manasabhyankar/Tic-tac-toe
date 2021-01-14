83d7b73e30bd14f099fa7d9819b5106c6e1e68d9
Manas Abhyankar <manasthemenace@gmail.com>
2021-01-14 13:34:34 -0800

    Added a lot of functionality and better README.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Tic-tac-toe
+
+## Create a functioning game of tic-tac-toe without frameworks.
+
+The goal of this project is to create tic-tac-toe without using any frameworks (like React, NodeJS, etc). This was created purely
+in HTML, CSS, and Javascript. It uses some basic formatting and colors to make things look nice.
+
+The assignment is a part of [TheOdinProject's](https://theodinproject.com/) web development tutorials.
+
+## TODO
+
+1. Indicate which player's turn it is.
+2. Keep score of wins and losses for each player.
+3. Allow the option to play again instead of having to resubmit names after each game.
--- tictactoe.js
+++ tictactoe.js
@@ -12,6 +12,7 @@ const GameBoard = (() => {
     let p1 = Player();
     let p2 = Player();
     let current_player = p1;
+    let player_selections = [];
     /*
      * Collect players' name and set shape.
      */
@@ -40,34 +41,97 @@ const GameBoard = (() => {
     const beginGame = () => {
         if(submit.getAttribute("data-clicked") == "0"){
             clearBoard();
+            submit.setAttribute("data-clicked", "1");
             attachCellListeners();
-        } 
+        }
+        if(current_player == p1){
+            current_player = p2;
+        }
+        else{
+            current_player = p1;
+        }
     }
     /*
      * Attach event listeners to cells in container. Will play animation when clicked, but only works when the game has begun. Changes player's turn.
      */
     const attachCellListeners = () => {
-        submit.setAttribute("data-clicked", "1");
         let cells = document.getElementById("cell-container").getElementsByTagName("*");
         for (let i=0; i<cells.length; i++){
             cells[i].setAttribute("data-clicked", "0");
-            cells[i].addEventListener('click', () => {
-                if(cells[i].getAttribute("data-clicked") == "0"){
-                    if(current_player == p1){
-                        cells[i].classList.add("cell-animated-p1");
-                        cells[i].classList.remove("cell");
-                        cells[i].setAttribute("data-clicked", "1");
-                        current_player = p2;
-                    }
-                    else if(current_player == p2){
-                        cells[i].classList.add("cell-animated-p2");
-                        cells[i].classList.remove("cell");
-                        cells[i].setAttribute("data-clicked", "1");
-                        current_player = p1;
-                    }
-                }
-            });
+            player_selections.push(i*10);
+            cells[i].addEventListener('click', cellBehavior);
         }
+    }
+    /**
+     * Defines cell behavior when clicked.
+     */
+    function cellBehavior(e) {
+        if(e.target.getAttribute("data-clicked") == "0"){
+            if(current_player == p1){
+                e.target.classList.add("cell-animated-p1");
+                e.target.classList.remove("cell");
+                e.target.setAttribute("data-clicked", "1");
+                player_selections[parseInt(e.target.id, 10)-1] = 1;
+                if(checkWinOrTie()){
+                    clearBoard();
+                    submit.setAttribute("data-clicked", "0");
+                }
+                current_player = p2;
+            }
+            else if(current_player == p2){
+                e.target.classList.add("cell-animated-p2");
+                e.target.classList.remove("cell");
+                e.target.setAttribute("data-clicked", "1");
+                player_selections[parseInt(e.target.id, 10)-1] = 2;
+                if(checkWinOrTie()){
+                    clearBoard();
+                    submit.setAttribute("data-clicked", "0");
+                }
+                current_player = p1;
+            }
+        }
+        console.log(player_selections);
+    }
+    /**
+     * Check if either player has won.
+     */
+    function checkWinOrTie(){
+        if((player_selections[0] == player_selections[1] == player_selections[2]) || 
+           (player_selections[0] == player_selections[3] == player_selections[6]) || 
+           (player_selections[0] == player_selections[4] == player_selections[8])){
+               if(player_selections[0] == 1){
+                   alert(p1.getName() + " has won!");
+                   return true;   
+               }
+               else{
+                   alert(p2.getName() + " has won!");
+                   return true;
+               }
+        }
+        if(player_selections[3] == player_selections[4] == player_selections[5] ||
+           player_selections[1] == player_selections[4] == player_selections[7] ||
+           player_selections[2] == player_selections[4] == player_selections[6]){
+            if(player_selections[3] == 1 || player_selections[1] == 1 || player_selections[2] == 1){
+                alert(p1.getName() + " has won!");
+                return true;   
+            }
+            else{
+                alert(p2.getName() + " has won!");
+                return true;
+            }
+        }
+        if(player_selections[6] == player_selections[7] == player_selections[8] ||
+           player_selections[2] == player_selections[5] == player_selections[8]){
+            if(player_selections[6] == 1 || player_selections[2] == 1){
+                alert(p1.getName() + " has won!");
+                return true;   
+            }
+            else{
+                alert(p2.getName() + " has won!");
+                return true;
+            }
+        }
+        return false;
     }
     /**
      * 
@@ -82,7 +146,9 @@ const GameBoard = (() => {
             if(!("cell" in cell_container[i].classList)){
                 cell_container[i].classList.add("cell");
             }
+            cell_container[i].removeEventListener("click", cellBehavior);
         }
+        player_selections = [];
         submit.setAttribute("data-clicked", "0");
         clearPlayerInfo();
     }
@@ -105,4 +171,3 @@ const GameBoard = (() => {
     let reset = document.getElementById("reset");
     reset.addEventListener("click", clearBoard);
 })();
-
